af445ac6a11601b5e6337c101fd71e83dbd70c57
Rafael <r-enc@hotmail.com>
2021-12-22 19:18:40 +0000

    Add New File

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for alpha in str:
+        if (ord(alpha) >= ord('a')) and (ord(alpha) <= ord('z')):
+            c = chr(ord(alpha)-ord('a')+ord('A'))
+        print("{}".format(c), end='')
+    print()
