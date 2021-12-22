cae2ee4ab96787ec048893cf2d5da0db414539d3
Rafael <r-enc@hotmail.com>
2021-12-22 15:21:53 +0000

    Add: New File

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for idx in range(1, 101):
         if idx % 3 == 0 and idx % 5 == 0:
-            print("FizzBuzz ", end="")
+            print("FizzBuzz", end=" ")
         elif idx % 3 == 0:
-            print("Fizz ", end="")
+            print("Fizz", end=" ")
         elif idx % 5 == 0:
-            print("Buzz ", end="")
+            print("Buzz", end=" ")
         else:
-            print("{}".format(idx), end="")
+            print("{}".format(idx), end=" ")
