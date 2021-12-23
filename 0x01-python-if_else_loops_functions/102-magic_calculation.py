5273d4e51e7c425246e39c5c7481078b18508b88
Rafael <r-enc@hotmail.com>
2021-12-23 04:15:39 +0000

    Add:New Task

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b, c):
     if a < b:
         return c
-    elif c > B:
+    elif c > b:
         return a + b
     else:
-        return (a * b) + c
+        return (a * b) - c
