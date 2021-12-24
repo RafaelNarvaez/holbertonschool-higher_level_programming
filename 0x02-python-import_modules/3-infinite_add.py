a136d163666fb118a9f268961e603a6d373c9ede
Rafael <r-enc@hotmail.com>
2021-12-24 04:00:37 +0000

    Add:New Task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == "__main__":
     argv = sys.argv
     idx = len(argv)
     result = 0
-    if idx - 1 ==0:
+    if idx - 1 == 0:
         print 0
     else:
         for idy in range(1, idx):
