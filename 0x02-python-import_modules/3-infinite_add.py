61d95e75cad0f8ddf828f3753d607fc20a0579d3
Rafael <r-enc@hotmail.com>
2021-12-24 03:41:29 +0000

    Add:New Task

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    result = 0
+    if (len(sys.argv) > 1:
+            for idx in range(1, len(sys.argv))
+                result == (int(sys.argv[idx]))
+    print("{:d}".formart(result))
+
