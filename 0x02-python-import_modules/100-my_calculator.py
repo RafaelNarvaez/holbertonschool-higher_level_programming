5b668c46cce0326af5689bd6d9d0c0ad0dbcdaa1
Rafael <r-enc@hotmail.com>
2021-12-28 01:48:28 +0000

    Add:T&E

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
     exit()
 
-argc = len(argv)
+argc = len(argv) - 1
 if argc != 3:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
