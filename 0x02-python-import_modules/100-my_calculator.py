0d2c545532f7696d1fcc57ba912a190dbca52aac
Rafael <r-enc@hotmail.com>
2021-12-28 01:57:26 +0000

    Add:T&E

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,9 +3,8 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
     exit()
-
 argc = len(argv) - 1
-if argc != 3:
+if argc != 4:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
 elif argv[2] == '+':
@@ -17,7 +16,7 @@ elif argv[2] == '*':
 elif argv[2] == '/':
     func = div
 else:
-    print("Unknown operator. Available operators: +, -, *, and / ")
+    print("Unknown operator. Available operators: +, -, *, and /")
     exit(1)
 
 result = func(int(argv[1]), int(argv[3]))
