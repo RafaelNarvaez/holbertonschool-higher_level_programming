ff3f7e1f7a2c02cb32540c56fa4154512704a256
Rafael <r-enc@hotmail.com>
2021-12-28 02:56:43 +0000

    Add:T&E

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
-
 if __name__ != "__main__":
     exit()
+
 argc = len(argv) - 1
 if argc != 3:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
-
-a = int(argv[1])
-b = int(argv[3])
-
 elif argv[2] == '+':
-    func = add(a, b)
-    print("{:d} {} {:d} = {:d}".format(a, argv[2], b, func)
+    func = add
 elif argv[2] == '-':
-    func = sub(a, b)
-    print("{:d} {} {:d} = {:d}".format(a, argv[2], b, func)
+    func = sub
 elif argv[2] == '*':
-    func = mul(a, b)
-    print("{:d} {} {:d} = {:d}".format(a, argv[2], b, func)
+    func = mul
 elif argv[2] == '/':
-    func = div(a, b)
-    print("{:d} {} {:d} = {:d}".format(a, argv[2], b, func)
+    func = div
 else:
     print("Unknown operator. Available operators: +, -, *, and /")
     exit(1)
+
+result = func(int(argv[1]), int(argv[3]))
+print("{:d} {:s} {:d} = {:d}".format(int(
+    argv[1]), argv[2], int(argv[3]), result))
