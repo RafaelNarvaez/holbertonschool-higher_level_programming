72766018954f50e51bcf837e39e79acc36abc8d3
Rafael <r-enc@hotmail.com>
2021-12-29 03:32:48 +0000

    Add:New Task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
 #!/usr/biun/python3
 def magic_calculation(a, b):
-    from calculator_1 import add, sub
+    from calculator_102 import add, sub
     if a < b:
         c = add(a, b)
         for idx in range(4, 6):
             c = add(c, idx)
-            return c
-        else:
-            return sub(a, b)
+        return c
+    else:
+        return sub(a, b)
