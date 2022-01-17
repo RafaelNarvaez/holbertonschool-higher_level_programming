b85d8b71d65179368e4c5e8188e6b3b85b8ee0c0
Rafael <r-enc@hotmail.com>
2022-01-17 18:35:39 +0000

    Add:Fixed indentation

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     print_int = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             print_int += 1
         except (TypeError, ValueError):
             continue
-        print("")
-        return print_int
+    print("")
+    return print_int
