6905b3815ba12c0a348a15eb17507f03c33df814
Rafael <r-enc@hotmail.com>
2022-01-25 03:54:28 +0000

    Add:New Task/Defines a Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,24 +14,6 @@ class Rectangle:
         self.width = width
 
     @property
-    def height(self):
-        """Retreive the value of height"""
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Set the value of height"""
-
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-
-        if value < 0:
-            raise TypeError("height must be >= 0")
-
-        self.__height = value
-
-    @property
     def width(self):
         """Retreive the value of width"""
 
@@ -48,3 +30,21 @@ class Rectangle:
             raise TypeError("width must be >= 0")
 
         self.__width = value
+
+    @property
+    def height(self):
+        """Retreive the value of height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height"""
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise TypeError("height must be >= 0")
+
+        self.__height = value
