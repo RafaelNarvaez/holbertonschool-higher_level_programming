22432aad8d903978e8ffe93ca756bb6bb41650be
Rafael <r-enc@hotmail.com>
2022-01-25 22:57:48 +0000

    Add:New Task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module Add Integer
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Add integer a, b
+    Raises:
+        TypeError: a or b are not int or float
+    """
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError('a must be an integer')
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
