0c677096bed6d1e877a233e374fea86096f636ac
Rafael <r-enc@hotmail.com>
2022-01-25 03:51:22 +0000

    Add:New Task/Defines a Rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""This is a class named as Rectangle"""
+
+
+class Rectangle:
+    """
+    Defines a Rectangle
+    Args:
+            width = Rectangle width (int)
+            height = Rectangle height (int)
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """Retreive the value of height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height"""
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise TypeError("height must be >= 0")
+
+        self.__height = value
+
+    @property
+    def width(self):
+        """Retreive the value of width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of width"""
+
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise TypeError("width must be >= 0")
+
+        self.__width = value
