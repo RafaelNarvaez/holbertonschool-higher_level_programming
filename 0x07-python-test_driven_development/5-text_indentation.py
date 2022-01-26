4a6c5ae05f249ef821d2f654543f009c745eb960
Rafael <r-enc@hotmail.com>
2022-01-26 16:30:43 +0000

    Add:New Task

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Test-driven development
+    text_indentation - Arg:
+        text: str
+"""
+
+
+def text_indentation(text):
+    """
+    Put double \n after :, ?, .
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = True
+    for c in text:
+        if c == " " and flag:
+            continue
+        elif flag:
+            flag = False
+        if c not in [".", ":", "?"]:
+            print(c, end="")
+            continue
+        elif not flag:
+            print(c, end="\n\n")
+            flag = True
