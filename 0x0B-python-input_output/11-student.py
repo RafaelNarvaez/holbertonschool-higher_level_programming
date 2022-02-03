bb38bf87009cb5e0f3262741d26d9bf4926339bf
Rafael <r-enc@hotmail.com>
2022-02-03 01:57:17 +0000

    Add:New Task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' Class Student '''
+
+
+class Student:
+    """ Define Student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' student representation of the Stident instance '''
+        if attrs is None:
+            return self.__dict__
+
+        new = {}
+
+        for attributes in attrs:
+            if attributes in self.__dict__:
+                new[attributes] = self.__dict__[attributes]
+        return new
+
+    def reload_from_json(self, json):
+        '''' Replaces all attributes of the Student instance '''
+        idx = self.__dict__
+        for item in json:
+            if item in self.__dict__:
+                idx[item] = json.get(item)
