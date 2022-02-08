6007614f63bf62b07a3fad7daa581ccae204a038
Rafael <r-enc@hotmail.com>
2022-02-08 03:34:26 +0000

    Fix identation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,14 +7,14 @@ class Rectangle(Base):
     ''' a class Rectangle that inherits from Base '''
 
 
-def __init__(self, width, height, x=0, y=0, id=None):
-    ''' Initialize an object '''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        ''' Initialize an object '''
 
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
-    super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
