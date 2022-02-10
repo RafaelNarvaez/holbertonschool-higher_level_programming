d48b25e6e82b5d03391d233c48f3712efcb23c16
Rafael <r-enc@hotmail.com>
2022-02-10 03:57:57 +0000

    Add:Tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,3 +15,46 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        dictionary = []
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as file:
+            if list_objs:
+                for objects in list_objs:
+                    dictionary.append(objects.to_dictionary())
+            file.write(cls.to_json_string(dictionary))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            new_rect = cls(1, 1)
+        if cls.__name__ == "Square":
+            new_rect = cls(1)
+        new_rect.update(**dictionary)
+        return(new_rect)
+
+    @classmethod
+    def load_from_file(cls):
+        try:
+            with open(cls.__name__ + "json", "r") as file:
+                return[cls.create(**dictionary) for dictionary
+                       in cls.from_json_string(file.read())]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def reset_nb_objects():
+        Base._Base__nb_objects = 0
