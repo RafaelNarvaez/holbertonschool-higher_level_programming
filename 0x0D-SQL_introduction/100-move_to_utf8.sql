60edd97d535e6e5641edde335309028b7a0421c8
Rafael <r-enc@hotmail.com>
2022-03-10 22:33:10 +0000

    Add:New Task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,4 @@
 -- converts hbtn_0c_0 DB to 
 -- utf8mb4 collate utf8mb4_unicode_ci
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name BLOB;
-ALTER TABLE first_table MODIFY VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER TABLE first_table CONVERT TO CHARACTER
+SET utf8mb4 COLLATE utf8mb4_unicode_ci;
