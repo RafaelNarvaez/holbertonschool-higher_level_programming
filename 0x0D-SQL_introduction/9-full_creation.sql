83f4ea400117be06ec0316cdf2b2daa0688efc88
Rafael <r-enc@hotmail.com>
2022-03-10 21:50:49 +0000

    Add:New Task

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- create a new table in hbtn_0c_ DB
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
