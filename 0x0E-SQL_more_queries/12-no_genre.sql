8bd794402159b9a4cdc924b22c58e6a56be9796a
Rafael <r-enc@hotmail.com>
2022-03-11 03:32:22 +0000

    Add:Fix missing i

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 -- that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON show_d = id
+LEFT JOIN tv_show_genres ON show_id = id
 WHERE tv_show_genres.GENRE_ID IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
