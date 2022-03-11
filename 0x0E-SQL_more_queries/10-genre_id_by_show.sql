b4b8b6bf8a90321ef81a2f2f2840a914c236f305
Rafael <r-enc@hotmail.com>
2022-03-11 03:09:52 +0000

    Add:Now Fix: _id to .id

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked
+Select tv_shows.title, tv_show_genres.genre_id From tv_shows
+Inner Join tv_shows_genres On tv_shows.id = tv_show_genres.show_id
+Order By tv_shows.title, tv_show_genres.genre_id;
