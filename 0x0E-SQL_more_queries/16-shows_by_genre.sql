42933cd617f2dbb614d0cabfa1f523320a5203e1
Rafael <r-enc@hotmail.com>
2022-03-11 04:04:00 +0000

    Add:New Task

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that  st all shows and genres 
+-- linked to a show form hbtn_0d_tvshows DB
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title, tv_genres.name;
