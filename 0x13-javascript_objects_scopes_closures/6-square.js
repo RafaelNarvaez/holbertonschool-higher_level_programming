2c2a873e15d26eac996e59e676776b009d59c052
Rafael <r-enc@hotmail.com>
2022-04-06 04:38:31 +0000

    Add:New Tsk

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
-const SquareDef = require('./5-square.js');
-module.exports = class Square extends SquareDef {
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
-      return this.print();
+      this.print();
     } else {
-      let linePrint = '';
-      for (let i = 0; i < this.width; i++) {
-        linePrint += c;
-      }
-      for (let j = 0; j < this.height; j++) {
-        console.log(linePrint);
-      }
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
