c368c29e790001867fbd531a34e28e0464c4d811
Rafael <r-enc@hotmail.com>
2022-04-06 14:49:12 +0000

    Add:R7D

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    for (let i = 0; i < this.width; i++) {
+      if (c === undefined) {
+        c = 'X';
+      }
+      console.log(c.repeat(this.height));
     }
   }
 };
