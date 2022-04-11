8c15fef64a02f7632a3596cc1861cdf56b8367a9
Rafael <r-enc@hotmail.com>
2022-04-11 03:19:42 +0000

    Add:New Task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -28,33 +28,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-© 2022 GitHub, Inc.
-Terms
-Privacy
-import sys
-import MySQLdb
-
-
-def main():
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
-
-    holder = connection.cursor()
-    userInput = sys.argv[4]
-    holder.execute("""SELECT id,name FROM states WHERE name = %s
-            ORDER by id ASC""", (userInput,))
-    rows = holder.fetchall()
-    for row in rows:
-        print(row)
-    holder.close()
-    connection.close()
-
-
-if __name__ == "__main__":
-    main()
