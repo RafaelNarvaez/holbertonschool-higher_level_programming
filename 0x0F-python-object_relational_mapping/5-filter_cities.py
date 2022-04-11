47eaa1249978b15e8ecfcacb8454b663dfd69640
Rafael <r-enc@hotmail.com>
2022-04-11 03:28:09 +0000

    Add:New Task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,17 +6,17 @@ import MySQLdb
 
 def main():
     connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        )
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            )
     state_name = sys.argv[4]
     holder = connection.cursor()
     holder.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (state_name,))
+            JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+            ORDER BY cities.id", (state_name,))
     rows = holder.fetchall()
     auxvar = ""
     for row in rows:
