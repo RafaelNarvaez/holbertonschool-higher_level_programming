6835e236116b9c16dffd751688232dc392baa680
RafaelNarvaez <r-enc@hotmail.com>
2022-04-26 21:07:57 -0400

    T&E

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """ If error code detected ask for status code """
 
-if __name__ == "__main__":
-    import urllib.request
-    import urllib.error
-    import sys
-    url = sys.argv[1]
+from sys import argv
+from urllib.request import urlopen
+from urllib.error import HTTPError
 
-    req = urllib.request.Request(url)
+
+if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(req) as response:
-            info = response.read()
-            print(info.decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-        
+        with urlopen(argv[1]) as response:
+            print(response.read().decode('utf8'))
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
